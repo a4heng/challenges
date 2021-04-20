24dfd10076805c9f70760aa759ca0e302dd16cca
Andy <a4heng@ucsd.edu>
2021-04-19 21:22:03 -0700

    non constructible change problem

--- algos/mergeIntervals.js
+++ algos/mergeIntervals.js
@@ -19,4 +19,27 @@ function mergeOverlappingIntervals(array) {
   return result;
 }
 
+//cleaner version
+function mergeOverlappingIntervals(array) {
+  // Write your code here.
+	const sortedIntervals = array.sort((a,b) => a[0] - b[0]);
+	const mergedIntervals = [];
+	let currentInterval = sortedIntervals[0];
+	mergedIntervals.push(currentInterval);
+	
+	for (const nextInterval of sortedIntervals){
+		const [_, currentIntervalEnd] = currentInterval;
+		const [nextIntervalStart, nextIntervalEnd] = nextInterval;
+		
+		if(currentIntervalEnd >= nextIntervalStart){
+			currentInterval[1] = Math.max(currentIntervalEnd, nextIntervalEnd);
+		}else{
+			currentInterval = nextInterval;
+			mergedIntervals.push(currentInterval);
+		}
+	}
+	
+  return mergedIntervals;
+}
+
 exports.mergeOverlappingIntervals = mergeOverlappingIntervals;
